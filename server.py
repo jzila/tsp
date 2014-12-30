a664e0868b20c684a708df5280346b2c49a27db2
John Zila <john@jzila.com>
2014-12-30 13:07:58 -0800

    Added host parameter to app to try to get it to run on Flynn

--- server.py
+++ server.py
@@ -39,4 +39,4 @@ def tsp():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     print "Running on port: %s" % port
-    app.run(port=port)
+    app.run(host='0.0.0.0', port=port)
